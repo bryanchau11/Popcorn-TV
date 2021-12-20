b1bff3668f20b4cfc5c2a3228edcba6b5ffc2fa4
bryanchau11 <cktnhat1234@gmail.com>
2021-12-19 17:52:57 -0700

    Complete youtube Model

--- src/components/TV/DetailTV.js
+++ src/components/TV/DetailTV.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link, useParams } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
 import ReactStars from "react-rating-stars-component";
 import "../../style/Detail.css";
 import "../../style/bootstrap.min.css";
@@ -249,194 +250,10 @@ function DetailTV() {
 
     setEpisode(parseInt(numStr.substring(1)));
   }
-  /*
-    <div classNameName="Detail">
-      <div classNameName="container p-0">
-        <SearchBar />
-        <div classNameName="container-fluid">
-          <div classNameName="row">
-            <NavigationMenu />
-            <main
-              role="main"
-              classNameName="col-md-9 ml-sm-auto col-lg-10 px-4 movie_list"
-              style={{ paddingTop: "50px" }}
-            >
-              {detailTV.length == 0 ? (
-                ""
-              ) : (
-                <div classNameName="movie_card" id="bright">
-                  <div classNameName="info_section">
-                    <div classNameName="movie_header">
-                      <img
-                        classNameName="locandina"
-                        src={detailTV[0].poster_path}
-                        alt=""
-                      />
-                      <h1>{detailTV[0].name}</h1>
-                      <h4>{detailTV[0].first_air_date}</h4>
-                      <h6>
-                        Number of seasons: {detailTV[0].number_of_seasons}
-                      </h6>
-                      <span classNameName="minutes">{detailTV[0].runtime} min</span>
-                      <p classNameName="type">{detailTV[0].genres}</p>
-                    </div>
-                    <br />
-                    <div classNameName="movie_desc">
-                      <p classNameName="text">{detailTV[0].overview}</p>
-                    </div>
-                  </div>
-                  <div>
-                    <img
-                      classNameName="blur_back bright_back"
-                      src={detailTV[0].poster_path}
-                      alt=""
-                    />
-                  </div>
-                </div>
-              )}
+  const [show, setShow] = useState(false);
 
-              <div
-                classNameName="movie_card"
-                id="bright"
-                style={{ width: "1000px", height: "700px" }}
-              >
-                <iframe
-                  title="movie"
-                  src={`https://www.2embed.ru/embed/tmdb/tv?id=${tvID}&s=${season}&e=${episode}`}
-                  width="1000"
-                  height="700"
-                  allow="fullscreen"
-                />
-              </div>
-              <select onChange={handleChange}>
-                {tvList
-                  ? tvList.map((item) => (
-                      <option value={item.label}>{item.label}</option>
-                    ))
-                  : ""}
-              </select>
-              <hr />
-
-              <div classNameName="pt-8 pb-2 mb-3 border-bottom">
-                <div classNameName="row">
-                  <h1>Cast</h1>
-                </div>
-                <div classNameName="scrollmenu">
-                  {cast
-                    ? cast.map((item) => (
-                        <div classNameName="card-view">
-                          <div classNameName="card-header">
-                            <Link to={`/detail/${item.id_cast}`}>
-                              <img
-                                src={item.profile_path}
-                                alt=""
-                                style={{ width: "180px" }}
-                              />
-                            </Link>
-                          </div>
-                          <div classNameName="card-movie-content">
-                            <div classNameName="card-movie-content-head">
-                              <h2 classNameName="card-movie-title">{item.name}</h2>
-                            </div>
-                            <div classNameName="card-movie-info">
-                              <div classNameName="movie-running-time">
-                                <div classNameName="text">Character</div>
-                                <span>{item.character}</span>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      ))
-                    : ""}
-                </div>
-              </div>
-
-              <hr />
-              <div>
-                <div>Favorite:</div>
-                <button
-                  classNameName="likedMovie"
-                  onClick={toggleLiked}
-                  type="button"
-                >
-                  {liked ? (
-                    <img
-                      src="https://img.icons8.com/color/24/000000/filled-like.png"
-                      alt=""
-                    />
-                  ) : (
-                    <img
-                      src="https://img.icons8.com/material-outlined/24/000000/like--v1.png"
-                      alt=""
-                    />
-                  )}
-                </button>
-                <div>Rating:</div>
-                <ReactStars
-                  count={5}
-                  onChange={ratingChanged}
-                  size={24}
-                  activeColor="#ffd700"
-                />
-                <div>Comment:</div>
-                <div classNameName="textarea">
-                  <textarea
-                    cols="110"
-                    rows="5"
-                    ref={textInput}
-                    placeholder="Add comment..."
-                  />
-                </div>
-                <div classNameName="btn">
-                  <button
-                    classNameName="btn btn-primary"
-                    onClick={addComment}
-                    type="submit"
-                  >
-                    Post
-                  </button>
-                </div>
-              </div>
-
-              {newComment.map((item) => (
-                <div classNameName="row">
-                  <div classNameName="col-sm-12">
-                    <hr />
-                    <div classNameName="row">
-                      <div classNameName="col-sm-3">
-                        <img
-                          src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/60/000000/external-user-interface-kiranshastry-solid-kiranshastry.png"
-                          alt=""
-                        />
-                        <div classNameName="review-block-name">{item.name}</div>
-                        <div classNameName="review-block-date">
-                          {item.date}
-                          <br />
-                          {item.hour}
-                        </div>
-                      </div>
-                      <div classNameName="col-sm-9">
-                        <div classNameName="review-block-rate">
-                          {[...Array(item.rating)].map(() => (
-                            <img
-                              src="https://img.icons8.com/fluency/24/000000/star.png"
-                              alt=""
-                            />
-                          ))}
-                        </div>
-                        <div classNameName="review-block-description">
-                          {item.comment}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </main>
-          </div>
-        </div>
-      </div>
-    </div> */
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       {detailTV.length === 0 ? (
@@ -507,57 +324,45 @@ function DetailTV() {
                       {detailTV[0].vote_average} / 10
                     </span>
                   </div>
+
                   <div className="col-xs-6">
                     <button
                       type="button"
                       className="btn btn-red btn-lg"
                       data-toggle="modal"
                       data-target="#myModal"
+                      onClick={handleShow}
                     >
                       Watch Trailer
                     </button>
-                    <div id="myModal" className="modal fade" role="dialog">
-                      <div className="modal-dialog">
-                        <div className="modal-content">
-                          <div className="modal-header">
-                            <button
-                              type="button"
-                              className="close"
-                              data-dismiss="modal"
-                            >
-                              &times;
-                            </button>
-                            <h4 className="modal-title">Modal Header</h4>
-                          </div>
-                          <div className="modal-body">
-                            <div className="video-container">
-                              <div>
-                                <iframe
-                                  title="x"
-                                  width="640"
-                                  height="360"
-                                  className="youtube"
-                                  src="https://www.youtube.com/"
-                                  allowfullscreen=""
-                                ></iframe>
-                              </div>
+                    <Modal show={show} onHide={handleClose} size="lg">
+                      <Modal.Header closeButton>
+                        <Modal.Title>Youtube</Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body>
+                        <div className="modal-body">
+                          <div className="video-container">
+                            <div>
+                              <iframe
+                                title="x"
+                                width="auto"
+                                height="auto"
+                                className="youtube"
+                                src="https://www.youtube.com/"
+                                allowfullscreen=""
+                              ></iframe>
                             </div>
                           </div>
-                          <div className="modal-footer">
-                            <button
-                              type="button"
-                              className="btn btn-default"
-                              data-dismiss="modal"
-                            >
-                              Close
-                            </button>
-                          </div>
                         </div>
-                      </div>
-                    </div>
+                      </Modal.Body>
+                    </Modal>
                   </div>
                   <div className="col-xs-6">
-                    <a href="/nowplaying" className="btn-lg btn-green">
+                    <a
+                      href="/nowplaying"
+                      className="btn-lg btn-green"
+                      style={{ pointerEvents: "none" }}
+                    >
                       View More
                     </a>
                   </div>
